2965c494b87765c045ed8aaa1c8bb9db99fd7ec1
Xpresence <alezzigrriff@gmail.com>
2019-02-28 00:26:48 +0300

    Added solutions

---
+++ SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+
+    public class SingleLinkedList<T> : IList<T>
+    {
+        private class Node<Type>
+        {
+            public Type Data { get; set; }
+            public Node<Type> Next { get; set; }
+        }
+
+        Node<T> head;
+        Node<T> tail; 
+        int count;
+
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                Node<T> node = GetNode(index);
+                return node.Data;
+            }
+            set
+            {
+                Node<T> node = GetNode(index);
+                node.Data = value;
+            }
+        }
+
+        //получение узла по индексу
+        private Node<T> GetNode(int index)
+        {
+            Node<T> node = head;
+            while (index > 0 && node.Next != null)
+            {
+                node = node.Next;
+                index--;
+            }
+
+            if (index == 0)
+            {
+                return node;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        //добавление
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>()
+            {
+                Data = data
+            };
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.Next = node;
+            }
+
+            tail = node;
+
+            count++;
+        }
+
+        //вставка по индексу
+        public void Insert(int index, T data)
+        {
+            Node<T> node = GetNode(index);
+
+            Node<T> newNode = new Node<T>()
+            {
+                Data = data,
+                Next = node.Next
+            };
+
+            node.Next = newNode;
+        }
+
+        //удаление
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        head = head.Next;
+
+                        if (head == null)
+                        {
+                            tail = null;
+                        }
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        //удаление по индексу
+        public void RemoveAt(int index)
+        {
+            
+            if (index > 0)
+            {
+                Node<T> node = GetNode(index - 1);
+                Node<T> node2 = GetNode(index);
+
+                if(node2.Next != null)
+                {
+                    node.Next = node2.Next;
+                }
+                else
+                {
+                    node.Next = null;
+                }
+            }
+            else
+            {
+                Node<T> node = head.Next;
+                head = node;
+            }
+            
+        }
+
+        //количество
+        public int Count => count;
+
+        //проверка на пустоту
+        public bool IsEmpty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+
+        //очистка
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        //содержание элемента
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        //индекс первого вхождения элемента
+        public int IndexOf(T data)
+        {
+            var index = 0;
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return index;
+                }
+
+                current = current.Next;
+                index++;
+            }
+
+            return -1;
+        }
+
+        //копирование из массива
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array.Length < Count + arrayIndex)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Node<T> node = head;
+            var i = 0;
+
+            while (node != null)
+            {
+                array[arrayIndex + i] = node.Data;
+                node = node.Next;
+                i++;
+            }
+        }
+
+        //добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>()
+            {
+                Data = data,
+                Next = head
+            };
+
+            head = node;
+
+            if (count == 0)
+            {
+                tail = head;
+            }
+
+            count++;
+        }
+        //реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ UnitTestSingleLinkedList/UnitTest.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleLinkedList;
+
+namespace UnitTestSingleLinkedList
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestSingleLinkedList_Add()
+        {
+            var list = new SingleLinkedList<int>();
+
+            Assert.IsTrue(list.IsEmpty == true);
+            Assert.IsTrue(list.Count == 0);
+
+            list.Add(0);
+
+            Assert.IsTrue(list.Count == 1 && list[0] == 0);
+            //Assert.IsTrue(list.Count == 1);
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_AppendFirst()
+        {
+            var list = new SingleLinkedList<int>();
+
+            list.Add(0);
+
+            Assert.IsTrue(list.Count == 1);
+            Assert.IsTrue(list[0] == 0);
+
+            list.AppendFirst(17);
+
+            Assert.IsTrue(list.Count == 2);
+            Assert.IsTrue(list[0] == 17);
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_Insert()
+        {
+            var list = new SingleLinkedList<int>();
+
+            list.Add(17);
+            list.Insert(0, 1);
+
+            Assert.IsTrue(list.Count == 1);
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_foreach()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(list.Count == 7);
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == data[index++]);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_CopyTo()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            var newData = new int[7];
+
+            list.CopyTo(newData, 0);
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == newData[index++]);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_Clear()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == data[index++]);
+            }
+
+            list.Clear();
+
+            Assert.IsTrue(list.Count == 0);
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_IndexOf()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(list.IndexOf(10) == -1);
+            Assert.IsTrue(list.IndexOf(1) == 0);
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_Contains()
+        {
+            var list = new SingleLinkedList<int>();
+            
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(!list.Contains(17));
+            Assert.IsTrue(list.Contains(3));
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_Remove()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            list.Remove(2);
+            Assert.IsTrue(!list.Contains(2));
+
+        }
+
+        [TestMethod]
+        public void TestSingleLinkedList_RemoveAt()
+        {
+            var list = new SingleLinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            list.RemoveAt(2);
+            Assert.IsTrue(!list.Contains(3));
+
+        }
+    }
+}
